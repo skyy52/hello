0572f625b10b8004bf98a63fa2018a129ab8e58f
skyy <ld@skyy.work>
2019-11-19 17:46:11 +0800

    first time

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * @author skyy
+ * @version 1.0
+ * @date 2019/11/19 17:29
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello~");
+    }
+}
